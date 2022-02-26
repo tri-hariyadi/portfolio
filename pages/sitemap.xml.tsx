f178ae252f6c016e90f6736f0e98d419f881e1e5
tri hariyadi <trihariyadi@Admins-Mac.local>
2022-02-26 13:43:56 +0700

    [Add] sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 		.readdirSync(
 			{
 				development: 'pages',
-				production: '/srv',
+				production: '/',
 			}[process.env.NODE_ENV]
 		)
 		.filter(staticPage => {
