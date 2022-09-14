616d1ba99a193e516623d9fc5a9b7df98b26ae0e
tri hariyadi <trihariyadi24@gmail.com>
2022-09-14 22:19:41 +0700

    deploy to gh pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+},
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
